71f22c93ef6bc9d3e6e9e07a2c16738d47583e85
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-09-17 10:29:40 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./index.css";
+import NewRegister from "./register";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="">
       <NavBar />
 
-      <Main />
+      <Register />
 
       <Footer />
     </div>
@@ -52,7 +53,7 @@ function Main() {
   return (
     <div className="main">
       <div>
-        <Register />
+        <NewRegister />
       </div>
       <div>
         <Evidence />
@@ -63,43 +64,189 @@ function Main() {
 
 function Register() {
   return (
-    <div className="register-container">
-      <div className="form-group">
-        <label htmlFor="name">Jméno</label>
-        <input type="text" id="name" placeholder="Jan" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="surname">Příjmení</label>
-        <input type="text" id="surname" placeholder="Novák" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password-register">Heslo</label>
-        <input type="password" id="password-register" placeholder="******" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password-control-register">Heslo kontrola</label>
-        <input
-          type="password"
-          id="password-control-register"
-          placeholder="*******"
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="age">Věk</label>
-        <input type="number" id="age" placeholder="26" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="telephon-number">Telefonní číslo</label>
-        <input type="tel" id="telephon-number" placeholder="555 774 777" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="insurance-number">Číslo pojištěnce</label>
-        <input type="text" id="Insurance-number" placeholder="55151518" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="kod-contract">Kód smlouvy</label>
-        <input type="text" id="kod-contract" placeholder="jnfjnkdfk" />
-      </div>
+    // <div className="register-container">
+    //   <div className="form-group">
+    //     <label htmlFor="name">Jméno</label>
+    //     <input type="text" id="name" placeholder="Jan" />
+    //   </div>
+    //   <div className="form-group">
+    //     <label htmlFor="surname">Příjmení</label>
+    //     <input type="text" id="surname" placeholder="Novák" />
+    //   </div>
+    //   <div className="form-group">
+    //     <label htmlFor="password-register">Heslo</label>
+    //     <input type="password" id="password-register" placeholder="******" />
+    //   </div>
+    //   <div className="form-group">
+    //     <label htmlFor="password-control-register">Heslo kontrola</label>
+    //     <input
+    //       type="password"
+    //       id="password-control-register"
+    //       placeholder="*******"
+    //     />
+    //   </div>
+    //   <div className="form-group">
+    //     <label htmlFor="age">Věk</label>
+    //     <input type="number" id="age" placeholder="26" />
+    //   </div>
+    //   <div className="form-group">
+    //     <label htmlFor="telephon-number">Telefonní číslo</label>
+    //     <input type="tel" id="telephon-number" placeholder="555 774 777" />
+    //   </div>
+    //   <div className="form-group">
+    //     <label htmlFor="insurance-number">Číslo pojištěnce</label>
+    //     <input type="text" id="Insurance-number" placeholder="55151518" />
+    //   </div>
+    //   <div className="form-group">
+    //     <label htmlFor="kod-contract">Kód smlouvy</label>
+    //     <input type="text" id="kod-contract" placeholder="jnfjnkdfk" />
+    //   </div>
+    // </div>
+
+    <div className="container">
+      <form method="post" autoComplete="on">
+        {/* First name */}
+        <div className="box">
+          <label htmlFor="firstName" className="fl fontLabel">
+            {" "}
+            First Name:{" "}
+          </label>
+          <div className="new iconBox">
+            <i className="fa fa-user" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              autoFocus="on"
+              required
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* First name */}
+
+        {/* Second name */}
+        <div className="box">
+          <label htmlFor="secondName" className="fl fontLabel">
+            {" "}
+            Second Name:{" "}
+          </label>
+          <div className="fl iconBox">
+            <i className="fa fa-user" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="text"
+              required
+              name="secondName"
+              placeholder="Last Name"
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* Second name */}
+
+        {/* Phone No. */}
+        <div className="box">
+          <label htmlFor="phone" className="fl fontLabel">
+            {" "}
+            Phone No.:{" "}
+          </label>
+          <div className="fl iconBox">
+            <i className="fa fa-phone-square" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="text"
+              required
+              name="phoneNo"
+              maxLength="10"
+              placeholder="Phone No."
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* Phone No. */}
+
+        {/* Email ID */}
+        <div className="box">
+          <label htmlFor="email" className="fl fontLabel">
+            {" "}
+            Email ID:{" "}
+          </label>
+          <div className="fl iconBox">
+            <i className="fa fa-envelope" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Email Id"
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* Email ID */}
+
+        {/* Password */}
+        <div className="box">
+          <label htmlFor="password" className="fl fontLabel">
+            {" "}
+            Password{" "}
+          </label>
+          <div className="fl iconBox">
+            <i className="fa fa-key" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Password"
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* Password */}
+
+        {/* Gender */}
+        <div className="box radio">
+          <label htmlFor="gender" className="fl fontLabel">
+            {" "}
+            Gender:{" "}
+          </label>
+          <input type="radio" name="Gender" value="Male" required /> Male &nbsp;
+          &nbsp; &nbsp; &nbsp;
+          <input type="radio" name="Gender" value="Female" required /> Female
+        </div>
+        {/* Gender */}
+
+        {/* Terms and Conditions */}
+        <div className="box terms">
+          <input type="checkbox" name="Terms" required /> &nbsp; I accept the
+          terms and conditions
+        </div>
+        {/* Terms and Conditions */}
+
+        {/* Submit Button */}
+        <div className="box" style={{ background: "#2d3e3f" }}>
+          <input
+            type="submit"
+            name="Submit"
+            className="submit"
+            value="SUBMIT"
+          />
+        </div>
+        {/* Submit Button */}
+      </form>
     </div>
   );
 }
@@ -134,7 +281,6 @@ function Evidence() {
     </div>
   );
 }
-
 
 function Footer() {
   return (
---
+++ src/register.js
@@ -0,0 +1,151 @@
+import "./newRegister.css";
+
+export default function NewRegister() {
+  return (
+    <div className="container">
+      <form method="post" autoComplete="on">
+        {/* First name */}
+        <div className="box">
+          <label htmlFor="firstName" className="fl fontLabel">
+            {" "}
+            First Name:{" "}
+          </label>
+          <div className="new iconBox">
+            <i className="fa fa-user" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              autoFocus="on"
+              required
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* First name */}
+
+        {/* Second name */}
+        <div className="box">
+          <label htmlFor="secondName" className="fl fontLabel">
+            {" "}
+            Second Name:{" "}
+          </label>
+          <div className="fl iconBox">
+            <i className="fa fa-user" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="text"
+              required
+              name="secondName"
+              placeholder="Last Name"
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* Second name */}
+
+        {/* Phone No. */}
+        <div className="box">
+          <label htmlFor="phone" className="fl fontLabel">
+            {" "}
+            Phone No.:{" "}
+          </label>
+          <div className="fl iconBox">
+            <i className="fa fa-phone-square" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="text"
+              required
+              name="phoneNo"
+              maxLength="10"
+              placeholder="Phone No."
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* Phone No. */}
+
+        {/* Email ID */}
+        <div className="box">
+          <label htmlFor="email" className="fl fontLabel">
+            {" "}
+            Email ID:{" "}
+          </label>
+          <div className="fl iconBox">
+            <i className="fa fa-envelope" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Email Id"
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* Email ID */}
+
+        {/* Password */}
+        <div className="box">
+          <label htmlFor="password" className="fl fontLabel">
+            {" "}
+            Password{" "}
+          </label>
+          <div className="fl iconBox">
+            <i className="fa fa-key" aria-hidden="true"></i>
+          </div>
+          <div className="fr">
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Password"
+              className="textBox"
+            />
+          </div>
+          <div className="clr"></div>
+        </div>
+        {/* Password */}
+
+        {/* Gender */}
+        <div className="box radio">
+          <label htmlFor="gender" className="fl fontLabel">
+            {" "}
+            Gender:{" "}
+          </label>
+          <input type="radio" name="Gender" value="Male" required /> Male &nbsp;
+          &nbsp; &nbsp; &nbsp;
+          <input type="radio" name="Gender" value="Female" required /> Female
+        </div>
+        {/* Gender */}
+
+        {/* Terms and Conditions */}
+        <div className="box terms">
+          <input type="checkbox" name="Terms" required /> &nbsp; I accept the
+          terms and conditions
+        </div>
+        {/* Terms and Conditions */}
+
+        {/* Submit Button */}
+        <div className="box" style={{ background: "#2d3e3f" }}>
+          <input
+            type="submit"
+            name="Submit"
+            className="submit"
+            value="SUBMIT"
+          />
+        </div>
+        {/* Submit Button */}
+      </form>
+    </div>
+  );
+}
