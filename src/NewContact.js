9e1f7066163984e80a2ab21cd6afb47aec96a484
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-09-19 20:30:31 +0200

    Add files via upload
    Contact section was upgraded, added responsivity and clered bugs.

--- src/NewContact.js
+++ src/NewContact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./newContact.css";
+import "./newContact.css"; // Ponecháme import tvojí stávajícího CSS
 
 export function NewContact() {
   return (
@@ -7,86 +7,54 @@ export function NewContact() {
       <h2 className="contact-title">Kontakt na pojišťovnu</h2>
       <div className="contact-box">
         <p className="contact-paragraph">
-          <span>Jana Nováková </span>
-          <i
-            className="fa-contact fa-phone-square-contact black-icon-contact"
-            aria-hidden="true"
-          ></i>
-          <span className="phonenumber-span-contact">+420 774 887 999</span>{" "}
-          <i
-            className="fa-contact fa-envelope-contact black-icon"
-            aria-hidden="true"
-          ></i>{" "}
-          <span className="email-span">
-            email: jirinanovakova@reactpojistovna.cz
+          <span className="fl fontLabel">Jana Nováková</span>
+          <i className="iconBox square-box fa fa-phone-square box-phone-contact"></i>
+          <span className="phone-nummer-contact">+420 774 887 999</span>
+          <i className="iconBox square-box  fa fa-envelope box-email-contact"></i>
+          <span className="email-contact">
+            jirinanovakova@reactpojistovna.cz
           </span>
         </p>
       </div>
       <div className="contact-box">
         <p className="contact-paragraph">
-          <span>Buka Zázračná </span>
-          <i
-            className="fa-contact fa-phone-square-contact black-icon-contact"
-            aria-hidden="true"
-          ></i>
-          <span className="phonenumber-span-contact">+420 774 887 998</span>{" "}
-          <i
-            className="fa fa-envelope-contact black-icon-contact"
-            aria-hidden="true"
-          ></i>
-          <span className="email-span">
-            email: jirinagorbalova@reactpojistovna.cz
+          <span className="fl fontLabel">Buka Zázračná</span>
+          <i className="iconBox square-box fa fa-phone-square box-phone-contact"></i>
+          <span className="phone-nummer-contact">+420 774 887 998</span>
+          <i className="iconBox square-box fa fa-envelope box-email-contact"></i>
+          <span className="email-contact">
+            jirinagorbalova@reactpojistovna.cz
           </span>
         </p>
       </div>
       <div className="contact-box">
         <p className="contact-paragraph">
-          <span>Falka Věčná </span>
-          <i
-            className="fa-contact fa-phone-square-contact black-icon-contact"
-            aria-hidden="true"
-          ></i>
-          <span className="phonenumber-span-contact">+420 774 887 997</span>{" "}
-          <i
-            className="fa fa-envelope-contact black-icon-contact"
-            aria-hidden="true"
-          ></i>
-          <span className="email-span">
-            email: jirinavecna@reactpojistovna.cz
+          <span className="fl fontLabel">Falka Věčná</span>
+          <i className="iconBox square-box fa fa-phone-square box-phone-contact"></i>
+          <span className="phone-nummer-contact">+420 774 887 997</span>
+          <i className="iconBox square-box fa fa-envelope box-email-contact"></i>
+          <span className="email-contact">jirinavecna@reactpojistovna.cz</span>
+        </p>
+      </div>
+      <div className="contact-box">
+        <p className="contact-paragraph">
+          <span className="fl fontLabel">Petra Konečná</span>
+          <i className="iconBox square-box fa fa-phone-square box-phone-contact"></i>
+          <span className="phone-nummer-contact">+420 774 887 996</span>
+          <i className="iconBox square-box fa fa-envelope box-email-contact"></i>
+          <span className="email-contact">
+            jirinakonecna@reactpojistovna.cz
           </span>
         </p>
       </div>
       <div className="contact-box">
         <p className="contact-paragraph">
-          <span>Petra Konečná </span>
-          <i
-            className="fa-contact fa-phone-square-contact black-icon-contact"
-            aria-hidden="true"
-          ></i>
-          <span className="phonenumber-span-contact">+420 774 887 996</span>{" "}
-          <i
-            className="fa fa-envelope-contact black-icon-contact"
-            aria-hidden="true"
-          ></i>
-          <span className="email-span">
-            email: jirinakonecna@reactpojistovna.cz
-          </span>
-        </p>
-      </div>
-      <div className="contact-box">
-        <p className="contact-paragraph">
-          <span>Jiřina Peřinová </span>
-          <i
-            className="fa fa-phone-square-contact black-icon"
-            aria-hidden="true"
-          ></i>
-          <span className="phonenumber-span-contact">+420 774 887 996</span>{" "}
-          <i
-            className="fa fa-envelope-contact black-icon-contact"
-            aria-hidden="true"
-          ></i>
-          <span className="email-span">
-            email: jirinaperinova@reactpojistovna.cz
+          <span className="fl fontLabel">Jiřina Peřinová</span>
+          <i className="iconBox square-box fa fa-phone-square box-phone-contact"></i>
+          <span className="phone-nummer-contact">+420 774 887 996</span>
+          <i className="iconBox square-box fa fa-envelope box-email-contact"></i>
+          <span className="email-contact">
+            jirinaperinova@reactpojistovna.cz
           </span>
         </p>
       </div>
