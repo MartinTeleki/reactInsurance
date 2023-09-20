d14dd2d42a3bf1ce502d7edb5a4968326c9ac510
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-09-20 23:19:30 +0200

    Add files via upload
    pagination was added.

--- src/Pojistenci.js
+++ src/Pojistenci.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./pojistenci.css";
 
 export default function Pojistenci() {
-  const pojistenciData = [
+  const insurenceInformation = [
     { jmeno: "Jan Novák", bydliste: "Praha" },
     { jmeno: "Marie Svobodová", bydliste: "Brno" },
     { jmeno: "Petr Dvořák", bydliste: "Ostrava" },
+    { jmeno: "Marie Svobodová", bydliste: "Brno" },
+    { jmeno: "Marie Svobodová", bydliste: "Brno" },
+    { jmeno: "Jan Novák", bydliste: "Praha" },
+    { jmeno: "Marie Svobodová", bydliste: "Brno" },
+    { jmeno: "Petr Dvořák", bydliste: "Ostrava" },
+    { jmeno: "Jan Novák", bydliste: "Praha" },
+    { jmeno: "Marie Svobodová", bydliste: "Brno" },
+    { jmeno: "Marie Svobodová", bydliste: "Brno" },
+    { jmeno: "Petr Dvořák", bydliste: "Ostrava" },
+    { jmeno: "Marie Svobodová", bydliste: "Brno" },
+    { jmeno: "Marie Svobodová", bydliste: "Brno" },
+    { jmeno: "Jan Novák", bydliste: "Praha" },
+    { jmeno: "Marie Svobodová", bydliste: "Brno" },
+    { jmeno: "Marie Svobodová", bydliste: "Brno" },
+    { jmeno: "Petr Dvořák", bydliste: "Ostrava" },
   ];
+  const pageRecords = 3;
+
+  const [actuallyPage, setAktualniStranka] = useState(1);
+
+  const amountPages = Math.ceil(insurenceInformation.length / pageRecords);
+
+  const prijdiNaStranku = (cisloStranky) => {
+    setAktualniStranka(cisloStranky);
+  };
+
+  const zacatekIndex = (actuallyPage - 1) * pageRecords;
+  const konecIndex = zacatekIndex + pageRecords;
+  const zobrazeniPojistenci = insurenceInformation.slice(
+    zacatekIndex,
+    konecIndex
+  );
 
   return (
     <div className="table-container">
@@ -29,7 +60,7 @@ export default function Pojistenci() {
         </div>
       </div>
       <div className="table">
-        {pojistenciData.map((pojistenec, index) => (
+        {zobrazeniPojistenci.map((pojistenec, index) => (
           <div className="table-row" key={index}>
             <div className="table-name">{pojistenec.jmeno}</div>
             <div className="table-name">{pojistenec.bydliste}</div>
@@ -39,6 +70,35 @@ export default function Pojistenci() {
             </div>
           </div>
         ))}
+      </div>
+      <div className="pagination">
+        <button
+          className={`pagination-item ${actuallyPage === 1 ? "disabled" : ""}`}
+          onClick={() => prijdiNaStranku(1)}
+        >
+          Předchozí
+        </button>
+        {Array.from({ length: amountPages }, (v, i) => i + 1).map(
+          (pageNumber) => (
+            <button
+              key={pageNumber}
+              className={`pagination-item ${
+                actuallyPage === pageNumber ? "active" : ""
+              }`}
+              onClick={() => prijdiNaStranku(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          )
+        )}
+        <button
+          className={`pagination-item ${
+            actuallyPage === amountPages ? "disabled" : ""
+          }`}
+          onClick={() => prijdiNaStranku(amountPages)}
+        >
+          Další
+        </button>
       </div>
     </div>
   );
