edf0f72f97d8d43f72921d7b61904f3f326182a8
MartinTeleki <134091172+MartinTeleki@users.noreply.github.com>
2023-09-21 19:14:10 +0200

    Add files via upload
    Editing information was added.

--- src/Pojistenci.js
+++ src/Pojistenci.js
@@ -9,6 +9,7 @@ export default function Pojistenci() {
   const [selectedPolicyholder, setSelectedPolicyholder] = useState(null);
   const pageRecords = 3;
   const [showDetails, setShowDetails] = useState(false);
+  const [editingPolicyholder, setEditingPolicyholder] = useState(null);
 
   useEffect(() => {
     const storedEvidence =
@@ -31,6 +32,17 @@ export default function Pojistenci() {
     setMergedData(mergedData);
     setEvidenceList(storedEvidence);
   }, []);
+
+  const editPolicyholder = (editedPolicyholder) => {
+    const updatedEvidenceList = evidenceList.map((policyholder) =>
+      policyholder.id === editedPolicyholder.id
+        ? editedPolicyholder
+        : policyholder
+    );
+    setEvidenceList(updatedEvidenceList);
+    localStorage.setItem("evidenceTEST", JSON.stringify(updatedEvidenceList));
+    setEditingPolicyholder(null);
+  };
 
   function handleDeleteEvidenceList(index) {
     const updatedEvidenceList = [...evidenceList];
@@ -126,6 +138,7 @@ export default function Pojistenci() {
           changeInsurencePagePlus={changeInsurencePagePlus}
           amountPages={amountPages}
           detailPolicyHolder={detailPolicyHolder}
+          setEditingPolicyholder={setEditingPolicyholder}
         />
       )}
 
@@ -138,6 +151,103 @@ export default function Pojistenci() {
           setShowDetails={setShowDetails}
         />
       )}
+
+      <div className="table-container">
+        {editingPolicyholder !== null && (
+          <PolicyholderEdit
+            policyholder={evidenceList[editingPolicyholder]}
+            editPolicyholder={editPolicyholder}
+            cancelEdit={() => setEditingPolicyholder(null)}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+function PolicyholderEdit({ policyholder, editPolicyholder, cancelEdit }) {
+  const [insuranceType, setInsuranceType] = useState(
+    policyholder.insuranceType
+  );
+  const [amount, setAmount] = useState(policyholder.amount);
+  const [insuranceSubject, setInsuranceSubject] = useState(
+    policyholder.insuranceSubject
+  );
+  const [validityFrom, setValidityFrom] = useState(policyholder.validityFrom);
+  const [validityTo, setValidityTo] = useState(policyholder.validityTo);
+  const [insuredAmount, setInsuredAmount] = useState(
+    policyholder.insuredAmount
+  );
+
+  const handleEdit = () => {
+    const editedPolicyholder = {
+      ...policyholder,
+      insuranceType,
+      amount,
+      insuranceSubject,
+      validityFrom,
+      validityTo,
+      insuredAmount,
+    };
+    editPolicyholder(editedPolicyholder);
+  };
+
+  return (
+    <div className="policyholder-edit">
+      <h3>Editovat informace o pojištění</h3>
+      <div className="form-group">
+        <label>Typ pojištění:</label>
+        <input
+          type="text"
+          value={insuranceType}
+          onChange={(e) => setInsuranceType(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Částka:</label>
+        <input
+          type="text"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Předmět pojištění:</label>
+        <input
+          type="text"
+          value={insuranceSubject}
+          onChange={(e) => setInsuranceSubject(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Platnost od:</label>
+        <input
+          type="text"
+          value={validityFrom}
+          onChange={(e) => setValidityFrom(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Platnost do:</label>
+        <input
+          type="text"
+          value={validityTo}
+          onChange={(e) => setValidityTo(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Výše pojištění:</label>
+        <input
+          type="text"
+          value={insuredAmount}
+          onChange={(e) => setInsuredAmount(e.target.value)}
+        />
+      </div>
+      <button className="btn-save" onClick={handleEdit}>
+        Uložit
+      </button>
+      <button className="btn-cancel" onClick={cancelEdit}>
+        Zrušit
+      </button>
     </div>
   );
 }
@@ -184,6 +294,7 @@ function PolicyholderForm({
   changeInsurencePagePlus,
   amountPages,
   detailPolicyHolder,
+  setEditingPolicyholder,
 }) {
   return (
     <div>
@@ -198,9 +309,11 @@ function PolicyholderForm({
           <div
             className={`table-row ${pojistenec.isHidden ? "hidden" : ""}`}
             key={pojistenec.id}
-            onClick={() => detailPolicyHolder(pojistenec.id)}
           >
-            <div className="table-cell">
+            <div
+              className="table-cell"
+              onClick={() => detailPolicyHolder(pojistenec.id)}
+            >
               <span className="names-nav">
                 {pojistenec.firstName} {pojistenec.lastName}
               </span>
@@ -210,7 +323,12 @@ function PolicyholderForm({
               <span className="names-nav"> {pojistenec.city}</span>
             </div>
             <div className="table-cell">
-              <button className="btn-editovat">Editovat</button>
+              <button
+                className="btn-editovat"
+                onClick={() => setEditingPolicyholder(pojistenec.id)}
+              >
+                Editovat
+              </button>
               <button
                 className="btn-odstranit"
                 onClick={() => handleDeleteEvidenceList(pojistenec.id)}
